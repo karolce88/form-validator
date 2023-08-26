a021a55db00c4b1053f1acca50a50a21b5682f3e
Karol <karolcwiklik@o2.pl>
2023-08-26 17:23:53 +0200

    ready

--- script.js
+++ script.js
@@ -6,34 +6,91 @@ const sendBtn = document.querySelector('.send')
 const clearBtn = document.querySelector('.clear')
 const popup = document.querySelector('.popup')
 
+const showError = (input, msg) => {
+	const formBox = input.parentElement 
+	const errorMsg = formBox.querySelector('.error-text') 
 
-
-const checkForm = input => {
-    input.forEach(el => {
-        
-    })
-
-    
-
-    if (input.value === '') {
-        console.log('blad');
-    }
+	formBox.classList.add('error') 
+	errorMsg.textContent = msg 
 }
 
-sendBtn.addEventListener('click', e => {
-    e.preventDefault()
+const clearError = (input) => {
+	const formBox = input.parentElement 
+	formBox.classList.remove('error')
+}
 
-    checkForm([username, pass, pass2, email])
+const checkForm = (input) => {
+	input.forEach((el) => {
+		if (el.value === '') {
+			showError(
+				el,
+				el.placeholder
+			) 
+		} else {
+			clearError(
+				el
+			) 
+		}
+	})
+}
 
+const checkLength = (input, min) => {
+	if (input.value.length < min) {
+		showError(input, `${input.previousElementSibling.innerText.slice(0, -1)} składa się z mniej niż ${min} znaków.`)
+	}
+}
+
+const checkPassword = (pass1, pass2) => {
+	if (pass1.value !== pass2.value) {
+		showError(pass2, 'Hasła do siebie nie pasują')
+	}
+}
+
+const checkEmail = (email) => {
+	const re =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ 
+
+	if (re.test(email.value)) {
+		clearError(email) 
+	} else {
+		showError(email, 'Niepoprawny adres e-mail') 
+	}
+}
+
+
+const checkErrors = () => {
+
+    const allInputs = document.querySelectorAll('.form-box');
+    let errorCount = 0; 
+
+    allInputs.forEach(el => {  
+        if(el.classList.contains('error')) {
+            errorCount++  
+        }
+    })  
+
+    if(errorCount === 0) {
+        popup.classList.add('show-popup')
+    }
+
+    console.log(errorCount);
+}
+
+sendBtn.addEventListener('click', (e) => {
+	e.preventDefault() 
+
+	checkForm([username, pass, pass2, email]); 
+	checkLength(username, 3);
+	checkLength(pass, 8); 
+	checkPassword(pass, pass2); 
+	checkEmail(email); 
+    checkErrors();
 })
 
-clearBtn.addEventListener('click', e => {
-    e.preventDefault()
-
-    [username, pass, pass2, email].forEach(el => {
-        el.value = ''
-    })
+clearBtn.addEventListener('click', (e) => {
+	e.preventDefault()
+	;[username, pass, pass2, email].forEach((el) => {
+		el.value = '' 
+		clearError(el) 
+	})
 })
-
-
-
